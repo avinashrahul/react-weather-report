479d38d135a5b70191b2398944483b2745f93499
avinashrahul.gudimet <avinashrahul.gudimet@dish.com>
2017-09-13 11:40:37 -0600

    prevent default

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -9,21 +9,28 @@ class SearchBar extends Component {
 
     // If we do not use bind below our component do not know
     // what is handleOnChange function, that is why we are binding to this Component
+    // else we get undefined method setState for null
     // If we use onChange={() => this.handleOnChange()} then no need to bind
     this.handleOnChange = this.handleOnChange.bind(this);
   }
 
   handleOnChange(event) {
     // You always get an event by default as argument since you are using
-    // onChange wvwnt handler below
-
-    // console.log(event.target.value);
+    // onChange event handler in render inout field
     this.setState({term: event.target.value})
+  }
+
+  handleSubmit(event) {
+    // if this function uses this. then you need to add bind it in constructor as well
+
+    // If we do not do a preventDefault when you click enter/submit button
+    // it will do a POST request sending out the entire form object to server
+    event.preventDefault();
   }
 
   render() {
     return(
-      <form className="input-group">
+      <form className="input-group" onSubmit={this.handleSubmit}>
         <input
           placeholder="Enter a city to check forecast.."
           className="form-control"
