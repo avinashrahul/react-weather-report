965e09b176e085b6849f3a55b20c52f15022bbb0
avinashrahul.gudimet <avinashrahul.gudimet@dish.com>
2017-09-26 09:38:34 -0600

    call weather api and display data

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,16 @@
-export default function(state = null, action) {
-  return state;
+// import constants from index.js
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch(action.type) {
+  case FETCH_WEATHER:
+    // We need to store history of weather report for multiple cities
+    // DO not modift state directly like - state.push(new payload)
+    // U can use concat - this returns new array in JS - state.concat(new payload)
+
+    // or use ES6 syntax like below
+    return [ action.payload.data, ...state ];
+
+  }
+  return state
 }
